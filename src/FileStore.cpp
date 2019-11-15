4be06b99c910e3e71fdec92ea49f6a2dbb4514c8
kallaballa <amir@viel-zu.org>
2019-11-15 19:16:20 +0100

    redesigned PatchObject I/O code

--- src/FileStore.cpp
+++ src/FileStore.cpp
@@ -1,31 +1,15 @@
 #include "FileStore.hpp"
+#include "PatFileWriter.hpp"
+#include "PatFileReader.hpp"
+#include "MetFileReader.hpp"
+#include "MetFileWriter.hpp"
+#include "RevFileWriter.hpp"
+#include "RevFileReader.hpp"
 #include <fstream>
 #include <sstream>
 #include <cassert>
 
 namespace patchscript {
-
-std::string readFully(std::ifstream& in) {
-    std::stringstream sstr;
-    sstr << in.rdbuf();
-     return sstr.str();
-}
-
-std::map<std::string, std::string> readKV(std::istream& in) {
-	std::map<std::string, std::string> kv;
-	std::string line;
-	while (std::getline(in, line)) {
-		std::istringstream is_line(line);
-		std::string key;
-		if (std::getline(is_line, key, '=')) {
-			std::string value;
-			if (std::getline(is_line, value)) {
-				kv[key] = value;
-			}
-		}
-	}
-	return kv;
-}
 
 FileStore::FileStore(const string& strDirectory) : fsDir_(strDirectory) {
 }
@@ -40,48 +24,29 @@ void FileStore::store(std::vector<PatchObject> list) {
 	for (const auto& po : list) {
 		fs::path outRev(po.name_ + ".rev");
 		fs::path outPathRev = fsDir_ / outRev;
+		std::ofstream ofsRev(outPathRev.string(), std::ios_base::app);
+		RevFileWriter revWriter(ofsRev);
 
 		if (po.name_ != lastName) {
 			fs::path outCode(po.name_ + ".pat");
 			fs::path outMeta(po.name_ + ".met");
-
 			fs::path outPathCode = fsDir_ / outCode;
 			fs::path outPathMeta = fsDir_ / outMeta;
+			std::ofstream ofsPat(outPathCode.string());
+			std::ofstream ofsMet(outPathMeta.string());
+			PatFileWriter patWriter(ofsPat);
+			MetFileWriter metWriter(ofsMet);
 
 			//write code to pat file
-			std::ofstream ofsCode(outPathCode.string());
-			ofsCode << po.code_;
-			ofsCode.close();
+			patWriter.write(po);
 
 			//write meta data met file
-			std::ofstream ofsMeta(outPathMeta.string());
-			ofsMeta << "revision=" << po.revision_ << '\n';
-			ofsMeta << "runtimeName=" << po.runtimeName_ << '\n';
-			ofsMeta << "runtimeVersion=" << po.runtimeVersion_ << '\n';
-			ofsMeta << "description=" << po.description_ << '\n';
-			ofsMeta << "date=" << po.date_ << '\n';
-			ofsMeta << "layout=" << po.layout_ << '\n';
-			ofsMeta << "parameters=" << po.parameters_ << '\n';
-			ofsMeta << "keyboardBindings=" << po.keyboardBindings_ << '\n';
-			ofsMeta << "midiBindings=" << po.midiBindings_ << '\n';
-			ofsMeta.close();
-
+			metWriter.write(po);
 			//prune the revisions file
 			std::ofstream ofsRev(outPathRev.string());
 			ofsRev.close();
 		} else {
-			//append each revision meta data and code terminated by "\x00"
-			std::ofstream ofsRev(outPathRev.string(), std::ios_base::app);
-			ofsRev << "revision=" << po.revision_ << '\n';
-			ofsRev << "runtimeName=" << po.runtimeName_ << '\n';
-			ofsRev << "runtimeVersion=" << po.runtimeVersion_ << '\n';
-			ofsRev << "description=" << po.description_ << '\n';
-			ofsRev << "date=" << po.date_ << '\n';
-			ofsRev << "layout=" << po.layout_ << '\n';
-			ofsRev << "parameters=" << po.parameters_ << '\n';
-			ofsRev << "keyboardBindings=" << po.keyboardBindings_ << '\n';
-			ofsRev << "midiBindings=" << po.midiBindings_ << '\n' << (char) 0;
-			ofsRev << po.code_  << '\n' << (char) 0;
+			revWriter.write(po);
 		}
 		lastName = po.name_;
 	}
@@ -92,51 +57,26 @@ void FileStore::retrieve(std::vector<PatchObject>& list) {
 	for (const auto & entry : fs::directory_iterator(fsDir_)) {
 		if(entry.path().extension().string()  == ".pat") {
 			PatchObject po;
+			po.name_ = entry.path().stem().string();
+
 			fs::path codePath(entry.path());
 			fs::path metaPath((entry.path().parent_path() / entry.path().stem()).string() + ".met");
 			fs::path revPath((entry.path().parent_path() / entry.path().stem()).string() + ".rev");
-			std::ifstream codeIfs(codePath.string());
-			std::ifstream metaIfs(metaPath.string());
-			std::ifstream revIfs(revPath.string());
+			std::ifstream ifsPat(codePath.string());
+			std::ifstream ifsMet(metaPath.string());
+			std::ifstream ifsRev(revPath.string());
+			PatFileReader patReader(ifsPat);
+			MetFileReader metReader(ifsMet);
+			RevFileReader revReader(po.name_, ifsRev);
 
-			po.name_ = entry.path().stem().string();
-			po.code_ = readFully(codeIfs);
-			auto metaMap = readKV(metaIfs);
-			po.revision_ = std::atoll(metaMap["revision"].c_str());
-			po.runtimeName_ = metaMap["runtimeName"];
-			po.runtimeVersion_ = metaMap["runtimeVersion"];
-			po.description_= metaMap["description"];
-			po.date_ = std::atoll(metaMap["date"].c_str());
-			po.layout_ = metaMap["layout"];
-			po.parameters_ = metaMap["parameters"];
-			po.keyboardBindings_ = metaMap["keyboardBindings"];
-			po.midiBindings_= metaMap["midiBindings"];
+			patReader.read(po);
+			metReader.read(po);
 			list.push_back(po);
 
-			while(revIfs) {
-				PatchObject poRev;
-				string metaRev;
-				if(!getline(revIfs, metaRev, (char)0))
-					break;
-				string codeRev;
-
-				if(!getline(revIfs, codeRev, (char)0))
-					break;
-				poRev.name_ = po.name_;
-				poRev.code_ = codeRev;
-
-				std::istringstream metaRevIs(metaRev);
-				auto metaRevMap = readKV(metaRevIs);
-				poRev.revision_ = std::atoll(metaRevMap["revision"].c_str());
-				poRev.runtimeName_ = metaRevMap["runtimeName"];
-				poRev.runtimeVersion_ = metaRevMap["runtimeVersion"];
-				poRev.description_= metaRevMap["description"];
-				poRev.date_ = std::atoll(metaRevMap["date"].c_str());
-				poRev.layout_ = metaRevMap["layout"];
-				poRev.parameters_ = metaRevMap["parameters"];
-				poRev.keyboardBindings_ = metaRevMap["keyboardBindings"];
-				poRev.midiBindings_= metaRevMap["midiBindings"];
+			PatchObject poRev;
+			while(revReader.read(poRev)) {
 				list.push_back(poRev);
+				poRev = PatchObject();
 			}
 		}
 	}
