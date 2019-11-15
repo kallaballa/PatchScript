8e6affb0b8405b54a724dd0953bb232f4ecb4373
kallaballa <amir@viel-zu.org>
2019-11-15 19:15:27 +0100

    new dependency: ZipLib

--- build_third.sh
+++ build_third.sh
@@ -4,5 +4,7 @@ set -e
 
 cd third/Tonic/
 make $@
+cd ../ZipLib/
+make $@
 cd ../lua-5.3.5/
 make $@ posix MYCFLAGS=-fPIC
--- clean_third.sh
+++ clean_third.sh
@@ -5,5 +5,7 @@ set -e
 
 cd third/Tonic/
 make clean
+cd ../ZipLib/
+make clean
 cd ../lua-5.3.5/
 make clean
