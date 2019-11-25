be565d31f497849939ed1b8f6c4b1d89fd327d37
kallaballa <amir@viel-zu.org>
2019-11-25 13:30:44 +0100

    don't build ziplib yet

--- build_third.sh
+++ build_third.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 set -e
+set -x
 
 cd third/Tonic/
 make $@
-cd ../ZipLib/
-make $@
+# we are not using ziplib yet
+#cd ../ZipLib/
+#make $@
 cd ../lua-5.3.5/
 make $@ posix MYCFLAGS=-fPIC
