3149a6147aa4c2b5e464d54bcca09c70a273c787
kallaballa <amir@viel-zu.org>
2021-02-04 07:42:57 +0100

    moved operators to source file

--- third/Tonic/src/Tonic/ControlComparison.cpp
+++ third/Tonic/src/Tonic/ControlComparison.cpp
@@ -14,6 +14,91 @@ namespace Tonic { namespace Tonic_{
     rhsGen_(ControlValue(0))
   {}
   
+  ControlEquals operator == (ControlGenerator lhs, ControlGenerator rhs){
+    ControlEquals c;
+    c.setLeft(lhs);
+    c.setRight(rhs);
+    return c;
+  }
+
+  ControlEquals operator == (ControlGenerator lhs, TonicFloat right){
+    ControlEquals c;
+    c.setLeft(lhs);
+    c.setRight(ControlValue(right));
+    return c;
+  }
+
+  ControlNotEquals operator != (ControlGenerator lhs, ControlGenerator rhs){
+    ControlNotEquals c;
+    c.setLeft(lhs);
+    c.setRight(rhs);
+    return c;
+  }
+
+  ControlNotEquals operator != (ControlGenerator lhs, TonicFloat right){
+    ControlNotEquals c;
+    c.setLeft(lhs);
+    c.setRight(ControlValue(right));
+    return c;
+  }
+
+  ControlGreaterThan operator > (ControlGenerator lhs, ControlGenerator rhs){
+    ControlGreaterThan c;
+    c.setLeft(lhs);
+    c.setRight(rhs);
+    return c;
+  }
+
+  ControlGreaterThan operator > (ControlGenerator lhs, TonicFloat right){
+    ControlGreaterThan c;
+    c.setLeft(lhs);
+    c.setRight(ControlValue(right));
+    return c;
+  }
+
+  ControlGreaterOrEqual operator >= (ControlGenerator lhs, ControlGenerator rhs){
+    ControlGreaterOrEqual c;
+    c.setLeft(lhs);
+    c.setRight(rhs);
+    return c;
+  }
+
+  ControlGreaterOrEqual operator >= (ControlGenerator lhs, TonicFloat right){
+    ControlGreaterOrEqual c;
+    c.setLeft(lhs);
+    c.setRight(ControlValue(right));
+    return c;
+  }
+
+  ControlLessThan operator < (ControlGenerator lhs, ControlGenerator rhs){
+    ControlLessThan c;
+    c.setLeft(lhs);
+    c.setRight(rhs);
+    return c;
+  }
+
+  ControlLessThan operator < (ControlGenerator lhs, TonicFloat right){
+    ControlLessThan c;
+    c.setLeft(lhs);
+    c.setRight(ControlValue(right));
+    return c;
+  }
+
+  ControlLessOrEqual operator <= (ControlGenerator lhs, ControlGenerator rhs){
+    ControlLessOrEqual c;
+    c.setLeft(lhs);
+    c.setRight(rhs);
+    return c;
+  }
+
+  ControlLessOrEqual operator <= (ControlGenerator lhs, TonicFloat right){
+    ControlLessOrEqual c;
+    c.setLeft(lhs);
+    c.setRight(ControlValue(right));
+    return c;
+  }
+
+
 } // Namespace Tonic_
   
   
